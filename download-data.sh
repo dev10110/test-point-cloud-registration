8da33d2b5f724a58e890584b55151a3881d235a5
Devansh Ramgopal Agrawal <devansh@umich.edu>
2023-01-23 13:05:06 -0500

    initial commit

---
+++ download-data.sh
@@ -0,0 +1,7 @@
+# wget -c -p ./data http://vision.princeton.edu/projects/2016/3DMatch/downloads/rgbd-datasets/sun3d-harvard_c11-hv_c11_2.zip
+
+# wget -c -p ./data http://vision.princeton.edu/projects/2016/3DMatch/downloads/rgbd-datasets/analysis-by-synthesis-apt1-kitchen.zip
+
+# wget -c -p ./data http://vision.princeton.edu/projects/2016/3DMatch/downloads/rgbd-datasets/bundlefusion-copyroom.zip
+#
+wget -c -p ./data http://vision.princeton.edu/projects/2016/3DMatch/downloads/rgbd-datasets/7-scenes-heads.zip
